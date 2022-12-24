90161542c341e1bd5ab8ff742b165ca3afb89c49
8irys <ryskulya777@gmail.com>
2022-12-24 21:01:42 +0600

    fixed invalid auth credential

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,20 +1,22 @@
 import UIKit
 import Flutter
-import Firebase
+import FirebaseMessaging
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-override func application(
+  override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-    ) -> Bool {
-       
-        GeneratedPluginRegistrant.register(with: self)
-        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    }
-
-      override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        let firebaseAuth = Auth.auth()
-        firebaseAuth.setAPNSToken(deviceToken, type: AuthAPNSTokenType.unknown)
-    }
-}
+  ) -> Bool {
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+  
+  override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    // NOTE: For logging
+    // let deviceTokenString = deviceToken.reduce("", {$0 + String(format: "%02X", $1)})
+    // print("==== didRegisterForRemoteNotificationsWithDeviceToken ====")
+    // print(deviceTokenString)
+    Messaging.messaging().apnsToken = deviceToken
+  }
+  }
